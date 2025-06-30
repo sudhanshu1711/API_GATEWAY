6dc06a4d78b78db7765e7863e631b0be57506fde
sudhanshu <sudhanshu2679@gmail.com>
2025-06-30 16:54:43 +0530

    setup API Gateway

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const morgan = require('morgan')
+const { createProxyMiddleware } = require('http-proxy-middleware')
+const ratelimit = require('express-rate-limit')
+const axios = require('axios')
+
+const app = express()
+
+const PORT = 3005
+
+const limiter = ratelimit({
+    windowMs: 2 * 60 * 1000, 
+    max:5
+})
+
+app.use(morgan('combined'))
+app.use(limiter)
+app.use('/bookingservice', async (req, res, next) => {
+  try {
+    const response = await axios.get('http://localhost:3001/api/v1/isAuthenticated', {
+      headers: {
+        'x-access-token': req.headers['x-access-token']
+      }
+    });
+
+    // Check if success = true in auth service response
+    if (response.data.success) {
+      console.log('Auth passed');
+      return next();
+    } else {
+      return res.status(401).json({
+        message: 'Authentication failed: token invalid'
+      });
+    }
+  } catch (error) {
+    // Catch axios error
+    const status = error.response?.status || 500;
+    const details = error.response?.data || error.message;
+
+    console.error('Auth failed:', details);
+
+    return res.status(status).json({
+      message: 'Authentication check failed',
+      error: details
+    });
+  }
+});
+
+
+
+app.use('/bookingservice',createProxyMiddleware({target:'http://localhost:3002',changeOrigin:true}))
+app.listen(PORT,()=>{
+    console.log(`API Gateway is running on port ${PORT}`)
+})
